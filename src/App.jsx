e71d8953490e553a96b2ddf61b17fa6855f839c6
Andrew Cruickshank <cruickshankatc@gmail.com>
2024-03-28 23:02:51 -0400

    changes for the game type selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import DillemaBox from './components/DillemaBox'
 import MenuMain from "./components/gameInit/MenuMain"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -8,7 +7,7 @@ function App() {
     <Router>
       <div className='min-h-screen bg-gray-700'>
         <Routes>
-          <Route path="/" element={<MenuMain />} />
+          <Route path="/select" element={<MenuMain />} />
           <Route path="/dilemma-box" element={<DillemaBox />} />
         </Routes>
       </div>
