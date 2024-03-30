d0ca2ea22af59b5bb4f205e4b92e959aff0b74be
Andrew Cruickshank <cruickshankatc@gmail.com>
2024-03-30 03:17:34 -0400

    Add buttons for mode selection

---
+++ src/components/MenuMain.jsx
@@ -0,0 +1,12 @@
+const MenuMain = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+        <button className="font-mono bg-gray-900 text-white font-bold mx-4 py-3 px-6 border-4 border-purple-500  border border-transparent hover:border-blue-500 transition duration-300"
+          >Single Player</button>
+        <button className="font-mono bg-gray-900 text-white font-bold mx-4 py-3 px-6 border-4 border-purple-500">
+          Multiplayer</button>
+    </div>
+  );
+};
+
+export default MenuMain;
