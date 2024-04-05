de7e1af94e10178e04452d3b89b5ffa5906ccba7
Andrew Cruickshank <cruickshankatc@gmail.com>
2024-04-04 22:36:17 -0400

    Implement Redux setup with persistor and Redux Toolkit

---
+++ src/store.jsx
@@ -0,0 +1,33 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from "./features/counter/counterSlice";
+import storage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, counterReducer)
+
+const store = configureStore({
+  reducer: {
+    counter: persistedReducer,
+  },
+  middleware: (getDefaultMiddleWare) =>
+  getDefaultMiddleWare({
+    serializableCheck: {
+      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  })
+})
+
+export const persistor = persistStore(store);
+
+export default store;
